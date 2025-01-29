0badc12320c7cd4a306c03e019ffdb3ad9a405ff
tim12367 <tim12367@ymail.com>
2025-01-29 22:17:17 +0800

    將密碼存於DB2中

--- src/main/java/uk/lazycat/spring_security_test/config/SecurityConfig.java
+++ src/main/java/uk/lazycat/spring_security_test/config/SecurityConfig.java
@@ -2,14 +2,19 @@ package uk.lazycat.spring_security_test.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -39,7 +44,7 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService() {
+	public UserDetailsService userDetailsService(DataSource dataSource) {
 		UserDetails user = User.withUsername("user")
 				.password("{noop}dummy")
 				.roles("USER")
@@ -50,6 +55,18 @@ public class SecurityConfig {
 				.roles("ADMIN")
 				.build();
 
-		return new InMemoryUserDetailsManager(user, admin);
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
+
+		return jdbcUserDetailsManager;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
 	}
 }
