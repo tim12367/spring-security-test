4da2b0f64d27dacac7d95f96cc8f1efe753d018d
tim12367 <tim12367@ymail.com>
2025-02-01 20:18:58 +0800

    測試JWT是否與ROLE註解相容

--- src/main/java/uk/lazycat/spring_security_test/controller/TodoController.java
+++ src/main/java/uk/lazycat/spring_security_test/controller/TodoController.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import jakarta.annotation.security.RolesAllowed;
 
@@ -31,14 +31,16 @@ public class TodoController {
 	@GetMapping("/users/{username}/todos")
 //	@PreAuthorize("hasRole('USER') and #username == authentication.name") // 檢核存取角色權限
 //	@PostAuthorize("returnObject.username == 'test123'") // 檢核回覆資料合法性
-//	@RolesAllowed({ "ADMIN", "USER" })
-	@Secured({"ROLE_ADMIN"})
-	public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
+	@RolesAllowed({ "ADMIN", "USER" })
+//	@Secured({ "ROLE_ADMIN" })
+	public Todo retrieveTodosForSpecificUser(@PathVariable String username, Authentication authentication) throws JsonProcessingException {
+		logger.debug("test authentication: " + authentication);
 		return TODOS_LIST.get(0);
 	}
 
 	@PostMapping("/users/{username}/todos")
-	public void createTodoForSpecificUser(@PathVariable String username, @RequestBody Todo todo) {
+	public void createTodoForSpecificUser(@PathVariable String username, @RequestBody Todo todo, Authentication authentication) throws JsonProcessingException {
+		logger.info("test authentication: " + authentication);
 		logger.info("Create {} for {}", todo, username);
 	}
 
